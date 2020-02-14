2c821f08b2a7474b696e42b073665beee22e786e
didierganthier <didierganthierperan@gmail.com>
2020-02-13 19:39:36 -0500

    Retrieve expense and income data

--- app/src/main/java/com/example/wallethome/ExpenseFragment.java
+++ app/src/main/java/com/example/wallethome/ExpenseFragment.java
@@ -3,29 +3,148 @@ package com.example.wallethome;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.wallethome.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExpenseFragment extends Fragment {
+public class ExpenseFragment extends Fragment
+{
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference mExpenseDatabase;
 
-    public ExpenseFragment() {
-        // Required empty public constructor
-    }
+    private RecyclerView recyclerView;
 
+    private TextView expenseTotalSum;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expense, container, false);
+        View myView = inflater.inflate(R.layout.fragment_expense, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        String uid = user.getUid();
+
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
+        recyclerView = myView.findViewById(R.id.recycler_id_expense);
+
+        expenseTotalSum = myView.findViewById(R.id.expense_txt_result);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mExpenseDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                int total_amount = 0;
+
+                for(DataSnapshot myDataSnapshot: dataSnapshot.getChildren())
+                {
+                    Data data = myDataSnapshot.getValue(Data.class);
+
+                    total_amount += data.getAmount();
+
+                    String stTotalAmount = String.valueOf(total_amount);
+
+                    expenseTotalSum.setText(stTotalAmount);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return myView;
     }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Data, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>(
+                Data.class,
+                R.layout.expense_recycler_data,
+                MyViewHolder.class,
+                mExpenseDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder myViewHolder, Data data, int i)
+            {
+                myViewHolder.setType(data.getType());
+                myViewHolder.setNote(data.getNote());
+                myViewHolder.setDate(data.getDate());
+                myViewHolder.setAmount(String.valueOf(data.getAmount()));
+            }
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+
+        View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        private void setType(String type)
+        {
+            TextView mType = mView.findViewById(R.id.type_txt_expense);
+            mType.setText(type);
+        }
+
+        private void setNote(String note)
+        {
+            TextView mNote = mView.findViewById(R.id.note_txt_expense);
+            mNote.setText(note);
+        }
+
+        private void setDate(String date)
+        {
+            TextView mDate = mView.findViewById(R.id.date_txt_expense);
+            mDate.setText(date);
+        }
+
+        private void setAmount(String amount)
+        {
+            TextView mAmount = mView.findViewById(R.id.amount_txt_expense);
+            String stamount = String.valueOf(amount);
+            mAmount.setText(stamount);
+        }
+    }
 }
--- app/src/main/java/com/example/wallethome/IncomeFragment.java
+++ app/src/main/java/com/example/wallethome/IncomeFragment.java
@@ -3,29 +3,148 @@ package com.example.wallethome;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.wallethome.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class IncomeFragment extends Fragment {
+public class IncomeFragment extends Fragment
+{
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
 
-    public IncomeFragment() {
-        // Required empty public constructor
-    }
+    private RecyclerView recyclerView;
 
+    private TextView incomeTotalSum;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_income, container, false);
+        View myView = inflater.inflate(R.layout.fragment_income, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        String uid = user.getUid();
+
+        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+
+        recyclerView = myView.findViewById(R.id.recycler_id_income);
+
+        incomeTotalSum = myView.findViewById(R.id.income_txt_result);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mIncomeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                int total_amount = 0;
+
+                for(DataSnapshot myDataSnapshot: dataSnapshot.getChildren())
+                {
+                    Data data = myDataSnapshot.getValue(Data.class);
+
+                    total_amount += data.getAmount();
+
+                    String stTotalAmount = String.valueOf(total_amount);
+
+                    incomeTotalSum.setText(stTotalAmount);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return myView;
     }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Data, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>(
+                Data.class,
+                R.layout.income_recycler_data,
+                MyViewHolder.class,
+                mIncomeDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder myViewHolder, Data data, int i)
+            {
+                myViewHolder.setType(data.getType());
+                myViewHolder.setNote(data.getNote());
+                myViewHolder.setDate(data.getDate());
+                myViewHolder.setAmount(String.valueOf(data.getAmount()));
+            }
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+
+        View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        private void setType(String type)
+        {
+            TextView mType = mView.findViewById(R.id.type_txt_income);
+            mType.setText(type);
+        }
+
+        private void setNote(String note)
+        {
+            TextView mNote = mView.findViewById(R.id.note_txt_income);
+            mNote.setText(note);
+        }
+
+        private void setDate(String date)
+        {
+            TextView mDate = mView.findViewById(R.id.date_txt_income);
+            mDate.setText(date);
+        }
+
+        private void setAmount(String amount)
+        {
+            TextView mAmount = mView.findViewById(R.id.amount_txt_income);
+            String stamount = String.valueOf(amount);
+            mAmount.setText(stamount);
+        }
+    }
 }
