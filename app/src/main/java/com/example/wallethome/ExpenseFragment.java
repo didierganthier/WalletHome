dd3e8a7d7eb5804b4f5b484903c932318a1e855c
didierganthier <didierganthierperan@gmail.com>
2020-02-16 14:01:43 -0500

    Update and delete data function added for expense

--- app/src/main/java/com/example/wallethome/ExpenseFragment.java
+++ app/src/main/java/com/example/wallethome/ExpenseFragment.java
@@ -1,6 +1,8 @@
 package com.example.wallethome;
 
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wallethome.Model.Data;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +29,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 
 /**
@@ -37,6 +47,17 @@ public class ExpenseFragment extends Fragment
 
     private TextView expenseTotalSum;
 
+    private EditText edtAmount, edtType, edtNote;
+
+    private Button btnUpdate, btnDelete;
+
+    private String type, note;
+    private int amount;
+
+    private String post_key;
+
+    private ProgressDialog progressDialog;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -48,6 +69,8 @@ public class ExpenseFragment extends Fragment
         FirebaseUser user = mAuth.getCurrentUser();
 
         String uid = user.getUid();
+
+        progressDialog = new ProgressDialog(getActivity());
 
         mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
 
@@ -101,12 +124,26 @@ public class ExpenseFragment extends Fragment
                 mExpenseDatabase
         ) {
             @Override
-            protected void populateViewHolder(MyViewHolder myViewHolder, Data data, int i)
+            protected void populateViewHolder(MyViewHolder myViewHolder, final Data data, final int i)
             {
                 myViewHolder.setType(data.getType());
                 myViewHolder.setNote(data.getNote());
                 myViewHolder.setDate(data.getDate());
                 myViewHolder.setAmount(String.valueOf(data.getAmount()));
+
+                myViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        post_key = getRef(i).getKey();
+
+                        type = data.getType();
+                        note = data.getNote();
+                        amount = data.getAmount();
+
+                        updateDataItem();
+                    }
+                });
             }
         };
         recyclerView.setAdapter(adapter);
@@ -146,5 +183,105 @@ public class ExpenseFragment extends Fragment
             String stamount = String.valueOf(amount);
             mAmount.setText(stamount);
         }
+    }
+
+    private void updateDataItem()
+    {
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myView = inflater.inflate(R.layout.update_data_item, null);
+        mydialog.setCancelable(false);
+        mydialog.setView(myView);
+
+        edtAmount = myView.findViewById(R.id.amount_edt);
+        edtType = myView.findViewById(R.id.type_edt);
+        edtNote = myView.findViewById(R.id.note_edt);
+
+        edtType.setText(type);
+        edtType.setSelection(type.length());
+
+        edtNote.setText(note);
+        edtNote.setSelection(note.length());
+
+        edtAmount.setText(String.valueOf(amount));
+        edtAmount.setSelection(String.valueOf(amount).length());
+
+        btnUpdate = myView.findViewById(R.id.btnUp_Update);
+        btnDelete = myView.findViewById(R.id.btnUp_Delete);
+
+        final AlertDialog dialog = mydialog.create();
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                progressDialog.setTitle("Updating Values");
+                progressDialog.setMessage("Please wait while we are updating your data");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                type = edtType.getText().toString().trim();
+                note = edtNote.getText().toString().trim();
+
+                String mdamount = String.valueOf(amount);
+
+                mdamount = edtAmount.getText().toString().trim();
+
+                int myAmount = Integer.parseInt(mdamount);
+
+                String mDate = DateFormat.getDateInstance().format(new Date());
+
+                Data data = new Data(myAmount, type, note, post_key, mDate);
+
+                mExpenseDatabase.child(post_key).setValue(data)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(getActivity(), "Data successfully updated", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(getActivity(), "Sorry, " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                                progressDialog.dismiss();
+                                dialog.dismiss();
+                            }
+                        });
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                progressDialog.setTitle("Updating Values");
+                progressDialog.setMessage("Please wait while we are updating your data");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                mExpenseDatabase.child(post_key).removeValue()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(getActivity(), "Data successfuly removed", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(getActivity(), "Sorry, " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                                progressDialog.dismiss();
+                                dialog.dismiss();
+                            }
+                        });
+            }
+        });
+
+        dialog.show();
     }
 }
